4218f7d1b3c9584840450204e124010ad281beee
Hanseul <ihanseul731@danawa.com>
2024-08-18 17:53:57 +0900

    first commit

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {//엔티티를 찾아줌 DAO랑 비슷하다고 생각하면 됨
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+}
---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    void testMember() {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+
+        //when
+        Long savedId = memberRepository.save(member);//저장
+        Member findMember = memberRepository.find(savedId);
+
+        //then (검증)
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember: " + findMember);
+        System.out.println("Member: " + member);
+
+
+    }
+}
